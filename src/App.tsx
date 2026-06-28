204d0c7fc88bd2fa83799d281c816183e9eac78e
Priyansh <electroniorje@gmail.com>
2026-06-28 22:13:49 +0530

    Component Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/Button/button";
+
 function App() {
   return (
     <>
       <h1>WELCOME TO ELIB DASHBOARD</h1>
+      <Button>Click Me!</Button>
     </>
   )
 }
