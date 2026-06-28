f4b2634a60871053c805822f4c5614ad129c7e8f
Priyansh <electroniorje@gmail.com>
2026-06-28 22:40:55 +0530

    Initialization Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/Button/button";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1>WELCOME TO ELIB DASHBOARD</h1>
-      <Button>Click Me!</Button>
+      <Button variant={"destructive"}>Click Me!</Button>
     </>
   )
 }
 
-export default App
+export default App;
