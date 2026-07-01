c5de4cba186597eaac64231f5463667c03ed0435
Priyansh <electroniorje@gmail.com>
2026-07-01 14:43:25 +0530

    HomePage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router/dom";
+import { TooltipProvider } from "@/components/ui/tooltip"
 import router from './router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <TooltipProvider>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </TooltipProvider>
 )
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import HomePage from "@/pages/HomePage";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage/>,
   },
+  {
+    path: "/login",
+    element: <Login/>
+  }, 
+  {
+    path: "/register",
+    element: <Register/>
+  }
 ]);
 
 export default router;
