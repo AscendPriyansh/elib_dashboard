86c760c9ba30b18ca2b0253fb7dfbf18933e7a58
Priyansh <electroniorje@gmail.com>
2026-07-02 13:46:46 +0530

    Protected Routes

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,13 +4,20 @@ import {
     SidebarInset,
     SidebarProvider,
 } from "@/components/ui/sidebar"
-import { Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router"
+import useTokenStore from "@/store"
 
 export const iframeHeight = "800px"
 
 export const description = "A sidebar with a header and a search form."
 
 function DashboardLayout() {
+    const token = useTokenStore(state => state.token);
+
+    if(!token) {
+        return <Navigate to={"/auth/login"} replace/>
+    }
+
     return (
         <div className="[--header-height:calc(--spacing(14))]">
             <SidebarProvider className="flex flex-col">
