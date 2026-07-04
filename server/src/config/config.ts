9a991a7077dc7cbbf8ac7ff028a41d62154705b6
Priyansh <electroniorje@gmail.com>
2026-07-04 17:59:26 +0530

    cloudinary setup

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,7 +6,10 @@ const _config = {
     port: process.env.PORT,
     db_uri: process.env.MONGO_CONNECTION_STRING,
     env: process.env.NODE_ENV,
-    jwt_secret: process.env.JWT_SECRET
+    jwt_secret: process.env.JWT_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET
 }
 
 export const config = Object.freeze(_config);
